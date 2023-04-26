0e96fde32b11f3533ab699b77a8fae828b1f26db
Shadow <shamunahmed38@gmail.com>
2023-04-26 21:40:50 +0600

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '@/styles/globals.css'
+import Head from 'next/head'
+import Script from 'next/script'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+    
+
+    
+
+    <Component {...pageProps} />
+    </>
+  )
+}

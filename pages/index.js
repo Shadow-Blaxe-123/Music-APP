9c31096f7d546ab31807b2f250492183f4fcc83e
Shadow <shamunahmed38@gmail.com>
2023-04-27 12:13:29 +0600

    Formatted the code

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from "next/head";
 
-
-
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Next.JS Music App</title>
-    </Head>
+      <Head>
+        <title>Next.JS Music App</title>
+      </Head>
     </>
-  )
+  );
 }
